ca2212e7882716d175e0e55b5ee1fe57dff92ce5
hamwid <hampus.widell@alleman.nu>
2019-01-07 10:48:51 +0100

    3

--- Shop.java
+++ Shop.java
@@ -8,5 +8,19 @@ public class Shop {
     Products pList  = new Products();
     pList.addEntry("Bike: Bounce", "Peak", 1500);
     pList.addEntry("Bike: Slim",   "Peak", 2500);
+
+    for(int i=0;i<cList.getEntries().size();i++){
+      System.out.println(
+          "Name: "+ cList.getEntries().get(i).firstName + " " +
+                    cList.getEntries().get(i).lastName + "\n" +
+          "Address: " + cList.getEntries().get(i).address + " " +
+          "Number: "  + cList.getEntries().get(i).number);
+    }
+    for(int i=0;i<pList.getEntries().size();i++){
+      System.out.println(
+          "Name: "  + pList.getEntries().get(i).name + " " +
+          "Prod.: " + pList.getEntries().get(i).producer + ", " +
+          "Price: " + pList.getEntries().get(i).price + "kr");
+    }
   }
 }
