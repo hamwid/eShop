be983591738e1379761ff427df8c00758e1b6750
hamwid <hampus.widell@alleman.nu>
2019-01-07 10:40:19 +0100

    1.3

---
+++ Customers.java
@@ -0,0 +1,16 @@
+//Customers.java
+
+import java.util.ArrayList;
+
+public class Customers {
+  public static ArrayList<Customer> customerList = new ArrayList<Customer>();
+
+  public void addEntry(String firstname, String lastname,
+                       String address,   String number){
+    Customer c = new Customer();
+    c.createEntry(firstname, lastname, address, number);
+  }
+  public ArrayList<Customer> getEntries(){
+    return customerList;
+  }
+}
