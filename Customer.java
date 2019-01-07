193e5c225d1d383ee951fcf11f7ff63574ac9b4a
hamwid <hampus.widell@alleman.nu>
2019-01-07 10:37:23 +0100

    1.2

---
+++ Customer.java
@@ -0,0 +1,28 @@
+//Customer.java
+
+public class Customer {
+  public String firstName;
+  public String lastName;
+  public String address;
+  public String number;
+
+  public void setFirstName(String input){
+    firstName = input;
+  }
+  public void setLastName(String input){
+    lastName = input;
+  }
+  public void setAddress(String input){
+    address = input;
+  }
+  public void setNumber(String input){
+    number = input;
+  }
+  public void createEntry(String inputFirstName, String inputLastName,
+                          String inputAddress, String inputNumber){
+    setFirstName(inputFirstName);
+    setLastName(inputLastName);
+    setAddress(inputAddress);
+    setNumber(inputNumber);
+  }
+}
