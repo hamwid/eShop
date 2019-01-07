0235275201322de5bc57ffb97def54ac87db70fb
hamwid <hampus.widell@alleman.nu>
2019-01-07 10:43:48 +0100

    1.5

---
+++ Products.java
@@ -0,0 +1,17 @@
+//Products.java
+
+import java.util.ArrayList;
+
+public class Products {
+  public static ArrayList<Product> productList = new ArrayList<Product>();
+
+  public void addEntry(String name, String producer,
+                       double price){
+    Product p = new Product();
+    p.createEntry(name, producer, price);
+    
+  }
+  public ArrayList<Product> getEntries(){
+    return productList;
+  }
+}
