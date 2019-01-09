faf676f4496b32d78a16d728670fb1fef29d951e
hamwid <hampus.widell@alleman.nu>
2019-01-09 12:05:19 +0100

    Example placed in main() - included comments

--- Shop.java
+++ Shop.java
@@ -38,24 +38,66 @@ public class Shop {
     }
   }
   public void printAllDeliveries(Shop eShop){
+    System.out.println("**** Queued deliveries *****");
     for(int i=0;i<eShop.dList.getQueued().size();i++){
       eShop.dList.getQueued().get(i).print();
       System.out.print('\n');
     }
+    System.out.println("****************************\n");
+    System.out.println("**** Ongoing deliveries ****");
+    for(int i=0;i<eShop.dList.getongoing().size();i++){
+      eShop.dList.getongoing().get(i).print();
+      System.out.print('\n');
+    }
+    System.out.println("****************************\n");
+    System.out.println("*** Completed deliveries ***");
+    for(int i=0;i<eShop.dList.getCompleted().size();i++){
+      eShop.dList.getCompleted().get(i).print();
+      System.out.print('\n');
+    }
+    System.out.println("****************************\n");
   }
 
   public static void main(String[] args){
     Shop eShop = new Shop();
+
+    //Create two customers
     eShop.createCustomer("Hampus", "Widell", "HomeRoad", "0701");
     eShop.createCustomer("Anna",   "Bell",   "MyStreet", "0704");
+
+    //Create two products
     eShop.createProduct("Bike: Bounce", "Peak", 1500);
     eShop.createProduct("Bike: Slim",   "Peak", 2500);
+
+    //"Hampus Widell" order one instance of each product
     eShop.createPurchase(eShop.cList.getEntries().get(0),
                          eShop.prList.getEntries().get(0));
+    eShop.createPurchase(eShop.cList.getEntries().get(0),
+                         eShop.prList.getEntries().get(1));
+    //"Anna Bell" order two instances of "Bike: Slim"
     eShop.createPurchase(eShop.cList.getEntries().get(1),
-                         eShop.prList.getEntries().get(0));
+                         eShop.prList.getEntries().get(1));
+    eShop.createPurchase(eShop.cList.getEntries().get(1),
+                         eShop.prList.getEntries().get(1));
 
+    //Each purchase gets placed in the delivery-queue
     eShop.createDelivery(eShop.puList.getEntries().get(0));
+    eShop.createDelivery(eShop.puList.getEntries().get(1));
+    eShop.createDelivery(eShop.puList.getEntries().get(2));
+    eShop.createDelivery(eShop.puList.getEntries().get(3));
+
+    //Send "Bike: Bounce" to "Hampus Widell", and remove from queue
+    eShop.dList.sendEntry(0);
+    //Send "Bike: Slim" to "Hampus Widell", and remove from queue
+    eShop.dList.sendEntry(0);
+    //Send "Bike: Slim" to "Anna Bell", and remove from queue
+    eShop.dList.sendEntry(0);
+    //  Second order of "Bike: Slim" from "Anna Bell" can't be sent
+    //  Item out of stock!
+
+    //"Bike: Bounce" is delivered to "Hampus Widell"
+    eShop.dList.completeEntry(0);
+
     eShop.printAllDeliveries(eShop);
   }
 }
